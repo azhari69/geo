55b5ea4e546cf4cde381da3a09cdeb36c656450d
azhari69 <43501198+azhari69@users.noreply.github.com>
2022-08-22 21:42:32 +0700

    kedua
    kedua

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+const host = 'https://geoportal.purworejokab.go.id';
+
+export const config = {
+  host: `${host}/`,
+  api: `${host}/api`,
+  wms: `${host}/geoserver/wms?`,
+};
+export default config;
